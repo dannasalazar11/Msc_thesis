b13047dc41087b78d461ed19a1e5caf46770dfe6
dannasalazar11 <93801258+dannasalazar11@users.noreply.github.com>
2026-04-10 08:21:28 -0500

    Refactor segmentar_senales to include window_ids

--- TGARNet/utils.py
+++ TGARNet/utils.py
@@ -1,94 +1,90 @@
-import numpy as np
 import os
+import numpy as np
 import scipy.io
 from sklearn.preprocessing import OneHotEncoder
-from copy import deepcopy
 
 def segmentar_senales(db, labels):
     """
     Divide las señales EEG en segmentos de 512 instantes con un traslape del 50%.
-    
+
     Args:
         db (dict): Diccionario donde las claves son los nombres de los sujetos y los valores
                    son matrices de forma CxT_i (C = canales, T_i = tiempo).
-    
+
     Returns:
-        dict: Nuevo diccionario con los segmentos de cada sujeto.
+        tuple:
+            - segmentos: array de segmentos
+            - y: array de etiquetas
+            - sbjs: lista de sujetos por segmento
+            - window_ids: lista con identificador de ventana por segmento
     """
-    segmentos_db = {}
     segmento_tamano = 512
-    paso = int(segmento_tamano * 0.5)  # Traslape del 50%
+    paso = int(segmento_tamano * 0.5)  # 50% overlap
     i = 0
-    
+
     segmentos = []
     y = []
     sbjs = []
-    
+    window_ids = []
+
     for sujeto, senal in db.items():
         C, T = senal.shape
-        
-        # Crear segmentos con traslape
+        window_count = 1
+
         for inicio in range(0, T - segmento_tamano + 1, paso):
             segmento = senal[:, inicio:inicio + segmento_tamano]
             segmentos.append(segmento)
             y.append(labels[i])
             sbjs.append(sujeto)
+            window_ids.append(f"Window {window_count}")
+            window_count += 1
 
         i += 1
-    return np.array(segmentos), np.array(y), sbjs
-    
-    
+
+    return np.array(segmentos), np.array(y), sbjs, window_ids
+
 
 def get_segmented_data():
     """
     Se tiene que agregar en kaggle la base de datos
     """
-    ruta_carpeta_TDAH = '/kaggle/input/ieee-tdah-control-database/ieee/ADHD_group'  
-    ruta_carpeta_control = '/kaggle/input/ieee-tdah-control-database/ieee/Control_group'  
-    
-    # Nombre de cada sujeto
+    ruta_carpeta_TDAH = '/kaggle/input/datasets/daprosero/mi-tdah-dataset/MI_TDAH_Dataset/TDAH/ieee/ADHD_group'
+    ruta_carpeta_control = '/kaggle/input/datasets/daprosero/mi-tdah-dataset/MI_TDAH_Dataset/TDAH/ieee/Control_group'
+
     sujetos_TDAH = [archivo[:-4] for archivo in os.listdir(ruta_carpeta_TDAH) if archivo.endswith('.mat')]
     sujetos_TDAH.pop()
     sujetos_control = [archivo[:-4] for archivo in os.listdir(ruta_carpeta_control) if archivo.endswith('.mat')]
-    
+
     diagnostico = {}
-    
+
     for sbj in sujetos_TDAH:
         diagnostico[sbj] = 1
-    
+
     for sbj in sujetos_control:
         diagnostico[sbj] = 0
-    
-    # organizamos los datos de los sujetos con TDAH en un diccionario
+
     eeg_tdah = {}
-    
-    for i in range(len(sujetos_TDAH)):
-        sbj = sujetos_TDAH[i]
-        mat_file_path = ruta_carpeta_TDAH+'/'+sbj+'.mat'
+    for sbj in sujetos_TDAH:
+        mat_file_path = ruta_carpeta_TDAH + '/' + sbj + '.mat'
         data = scipy.io.loadmat(mat_file_path)
         columna = list(data.keys())[-1]
         eeg_tdah[sbj] = data[columna].T
-    
-    # organizamos los datos de los sujetos de control en un diccionario
+
     eeg_control = {}
-    
-    for i in range(len(sujetos_control)):
-        sbj = sujetos_control[i]
-        mat_file_path = ruta_carpeta_control+'/'+sbj+'.mat'
+    for sbj in sujetos_control:
+        mat_file_path = ruta_carpeta_control + '/' + sbj + '.mat'
         data = scipy.io.loadmat(mat_file_path)
         columna = list(data.keys())[-1]
         eeg_control[sbj] = data[columna].T
-    
+
     db = eeg_control | eeg_tdah
     zeros = np.zeros(len(eeg_control))
     ones = np.ones(len(eeg_tdah))
     labels = np.hstack((zeros, ones))
-    
-    X, y, sbjs = segmentar_senales(db, labels)
-    
+
+    X, y, sbjs, window_ids = segmentar_senales(db, labels)
+
     encoder = OneHotEncoder(sparse_output=False)
-    
-    # X = np.expand_dims(X, axis=-1)
     y = encoder.fit_transform(y.reshape(-1, 1))
 
-    return X, y, sbjs
+    return X, y, sbjs, window_ids
